7f92a67d5c612692ef0114f48e44c02c6b6b09ca
Soyoon Jeong <96245651+soyoonJ@users.noreply.github.com>
2022-05-06 19:08:52 +0900

    ✅ 섹션12_타입추론

---
+++ class-note/9_type-inference.ts
@@ -0,0 +1,45 @@
+// 타입 추론 기본 1
+// 값을 할당하면 변수 타입이 추론됨
+var a = 10;
+
+// 파라미터에 b가 없으면 10이 할당 => 10 할당될 경우 b가 number라고 추론
+function getB(b = 10) {
+    var c = 'hi'
+    return b+c;
+}
+
+10 + '10' // 1010
+
+
+// 타입 추론 기본 2
+// 인터페이스 & 제네릭
+// interface Dropdown<T> {
+//     value: T;
+//     title: string;
+// }
+// var shoppingItem: Dropdown<string> = {
+//     value: 'abc',
+//     title: 'hello'
+// }
+
+
+// 타입 추론 기본 3
+// 제네릭 연달아 두 개 나왔을 경우
+interface Dropdown<T> {
+    value: T;
+    title: string;
+}
+interface DetailedDropdown<K> extends Dropdown<K> {
+    description: string;
+    tag: K;
+}
+
+var detailedItem: DetailedDropdown<string> = {
+    title: 'hello',
+    description: 'ab',
+    value: 'a',
+    tag: 'a',
+}
+
+// Best Common Type
+var arr = [1, 2, true, true, 'a'];
